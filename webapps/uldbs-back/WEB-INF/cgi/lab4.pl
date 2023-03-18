870a5640c7540cd90796f8583311e7efbced0faa
aiko135 <pon4ik2282211@gmail.com>
2023-03-18 16:45:56 +0300

    Configured t tomee.xml added docker ip to jdbc source

--- webapps/uldbs-back/WEB-INF/cgi/lab4.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/perl
-
-print "Content-type: text/html\n\n";
-print "<font size=+1>Environment vars of PERL through the CGI gateway</font><br><br>\n";
-foreach (sort keys %ENV) {
-   print "<b>$_</b>: $ENV{$_}<br>\n";
-}
